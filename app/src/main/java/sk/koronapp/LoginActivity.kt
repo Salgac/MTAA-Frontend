9d18c4d862e9801cb537be1d87168ec834feb0f0
Dominik <domco.salgovic@gmail.com>
2021-04-07 13:00:11 +0200

    - fixed register button not working

--- app/src/main/java/sk/koronapp/LoginActivity.kt
+++ app/src/main/java/sk/koronapp/LoginActivity.kt
@@ -1,10 +1,16 @@
 package sk.koronapp
 
-import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.widget.doAfterTextChanged
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
@@ -12,8 +18,13 @@ import kotlinx.android.synthetic.main.activity_login.*
 import org.json.JSONObject
 import sk.koronapp.models.User
 import java.io.Serializable
+import java.util.*
+import kotlin.collections.HashMap
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), LocationListener {
+
+    private var address: String = "none"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -63,6 +74,11 @@ class LoginActivity : AppCompatActivity() {
         val jsonObj = JSONObject()
         jsonObj.put("username",username)
         jsonObj.put("password",password)
+        if(type=="register"){
+            updateAddress()
+            jsonObj.put("address",this.address)
+            Toast.makeText(this, this.address, Toast.LENGTH_LONG).show()
+        }
 
         val url= "http://139.162.130.177:8000/api/$type/"
 
@@ -97,6 +113,50 @@ class LoginActivity : AppCompatActivity() {
         }
 
         que.add(jsonObjectRequest)
+    }
+
+    //updates location and address line
+    private fun updateAddress(){
+        //check permissions
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            //ask for permissions
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),2)
+            return updateAddress()
+        }
+
+        //update location
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,5f,this)
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        if (location != null) {
+            //get current address from location and return in into variable
+            val geocoder = Geocoder(this, Locale.getDefault())
+            val addresses = geocoder.getFromLocation(location.latitude,location.longitude,1)
+
+            this.address = addresses[0].getAddressLine(0)
+        }
+        else{
+            //handle error
+            Toast.makeText(this, "Location error", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+        TODO("Not yet implemented")
     }
 
 }
