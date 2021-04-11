0df30bc79e99870e0314e833a833292317327704
Dominik <domco.salgovic@gmail.com>
2021-04-12 00:40:56 +0200

    - inserted user info into drawer

--- app/src/main/java/sk/koronapp/MainActivity.kt
+++ app/src/main/java/sk/koronapp/MainActivity.kt
@@ -2,6 +2,7 @@ package sk.koronapp
 
 import android.os.Bundle
 import android.view.Menu
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -11,7 +12,13 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.drawer_layout.*
+import kotlinx.android.synthetic.main.drawer_layout.view.*
+import sk.koronapp.models.User
+import sk.koronapp.utilities.HttpRequestManager
+import sk.koronapp.utilities.Urls
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val drawerNavView: NavigationView = findViewById(R.id.drawer_nav_view)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
@@ -37,10 +45,26 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        setDrawerValues(drawerNavView)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun setDrawerValues(view: NavigationView) {
+
+        val user: User = intent.getSerializableExtra("user") as User
+
+        val header = view.getHeaderView(0)
+        val imageLoader = HttpRequestManager.getImageLoader(this)
+
+        header.drawer_image.setImageUrl(Urls.AVATAR + user.avatar, imageLoader)
+        header.drawer_name.text = user.username
+        header.drawer_address.text = user.address
+
+
     }
 }
